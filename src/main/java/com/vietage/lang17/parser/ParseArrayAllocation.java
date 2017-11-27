0efa74b53e7e5d963b0cd79d6012c398fe99fda1
vietage <vietopk@gmail.com>
2017-11-27 22:27:48 +0300

    implemented ParseArrayAllocation

--- src/main/java/com/vietage/lang17/parser/ParseArrayAllocation.java
+++ src/main/java/com/vietage/lang17/parser/ParseArrayAllocation.java
@@ -1,6 +1,8 @@
 package com.vietage.lang17.parser;
 
 import com.vietage.lang17.lexer.lexeme.NewArray;
+import com.vietage.lang17.parser.ast.Type;
+import com.vietage.lang17.parser.ast.expression.ArrayAllocation;
 import com.vietage.lang17.parser.ast.expression.Expression;
 
 import java.util.Queue;
@@ -15,6 +17,17 @@ public class ParseArrayAllocation extends ParseCommand
 
     @Override
     public void parse(Queue<ParseCommand> commandQueue) {
+        Type type = Type.getArrayType(lexeme.getBasicType().getType());
 
+        ArrayAllocation arrayAllocation = new ArrayAllocation(type);
+
+        commandQueue.add(
+                new ParseExpression(
+                        lexeme.getIndexExpression().getExpression(),
+                        arrayAllocation::setSizeExpression
+                )
+        );
+
+        resultConsumer.consume(arrayAllocation);
     }
 }
--- src/main/java/com/vietage/lang17/parser/ast/expression/ArrayAllocation.java
+++ src/main/java/com/vietage/lang17/parser/ast/expression/ArrayAllocation.java
@@ -5,5 +5,21 @@ import com.vietage.lang17.parser.ast.Type;
 public class ArrayAllocation extends Expression {
 
     private final Type type;
-    private final Expression sizeExpression;
+    private Expression sizeExpression;
+
+    public ArrayAllocation(Type type) {
+        this.type = type;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Expression getSizeExpression() {
+        return sizeExpression;
+    }
+
+    public void setSizeExpression(Expression sizeExpression) {
+        this.sizeExpression = sizeExpression;
+    }
 }
