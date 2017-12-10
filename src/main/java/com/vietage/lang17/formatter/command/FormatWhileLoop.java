a42e02da6035f504df6771b1a9d1f7ff5c319a03
vietage <vietopk@gmail.com>
2017-12-10 17:43:57 +0300

    implemented while loop formatter

--- src/main/java/com/vietage/lang17/formatter/command/FormatWhileLoop.java
+++ src/main/java/com/vietage/lang17/formatter/command/FormatWhileLoop.java
@@ -8,6 +8,7 @@ import java.util.Deque;
 public class FormatWhileLoop extends FormatCommand {
 
     private final WhileLoop whileLoop;
+    private int invocationCount = 0;
 
     public FormatWhileLoop(int indent, WhileLoop whileLoop) {
         super(indent);
@@ -17,5 +18,34 @@ public class FormatWhileLoop extends FormatCommand {
     @Override
     public void format(IndentPrintStream out, Deque<FormatCommand> commands) {
 
+        switch (invocationCount) {
+            case 0:
+                invocationCount++;
+                commands.push(this);
+
+                out.print("while (", indent);
+
+                commands.push(new FormatExpression(indent, whileLoop.getCondition()));
+
+                break;
+            case 1:
+                invocationCount++;
+                commands.push(this);
+
+                out.println(")", indent);
+                out.println("{", indent);
+
+                commands.push(new FormatStatements(indent + 4, whileLoop.getStatements()));
+
+                break;
+            case 2:
+                invocationCount++;
+
+                out.println("}", indent);
+
+                break;
+            default:
+                throw new RuntimeException("format() method invoked too many times");
+        }
     }
 }
