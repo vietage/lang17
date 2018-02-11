c81c0b1a2ce0da07d2e0f9fe2a7d9abf886ec2cd
vietage <vietopk@gmail.com>
2018-02-11 12:10:55 +0300

    implemented FormatUnaryExpression

--- src/main/java/com/vietage/lang17/formatter/command/FormatUnaryExpression.java
+++ src/main/java/com/vietage/lang17/formatter/command/FormatUnaryExpression.java
@@ -7,12 +7,23 @@ import java.util.Queue;
 
 public class FormatUnaryExpression extends FormatCommand {
 
+    private final UnaryExpression unaryExpression;
+
     public FormatUnaryExpression(int indent, UnaryExpression unaryExpression) {
         super(indent);
+        this.unaryExpression = unaryExpression;
     }
 
     @Override
     public void format(IndentPrintStream out, Queue<FormatCommand> commandQueue) {
-
+        switch (unaryExpression.getOperator()) {
+            case NOT:
+                out.print("!", indent);
+                break;
+            case MINUS:
+                out.print("-", indent);
+                break;
+        }
+        commandQueue.add(new FormatExpression(indent, unaryExpression.getExpression()));
     }
 }
