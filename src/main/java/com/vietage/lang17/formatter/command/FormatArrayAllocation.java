00dd9603f46b87148de3763815d28d8d695a2a63
Alexey Shilov <vietopk@gmail.com>
2018-02-17 13:11:48 +0300

    implemented FormatArrayAllocation

--- src/main/java/com/vietage/lang17/formatter/command/FormatArrayAllocation.java
+++ src/main/java/com/vietage/lang17/formatter/command/FormatArrayAllocation.java
@@ -7,12 +7,18 @@ import java.util.Queue;
 
 public class FormatArrayAllocation extends FormatCommand {
 
+    private final ArrayAllocation arrayAllocation;
+
     public FormatArrayAllocation(int indent, ArrayAllocation arrayAllocation) {
         super(indent);
+        this.arrayAllocation = arrayAllocation;
     }
 
     @Override
     public void format(IndentPrintStream out, Queue<FormatCommand> commandQueue) {
-
+        commandQueue.add(new FormatType(indent, arrayAllocation.getType()));
+        commandQueue.add(new InsertText(indent, "["));
+        commandQueue.add(new FormatExpression(indent, arrayAllocation.getSizeExpression()));
+        commandQueue.add(new InsertText(indent, "]"));
     }
 }
--- src/main/java/com/vietage/lang17/formatter/command/FormatArrayRead.java
+++ src/main/java/com/vietage/lang17/formatter/command/FormatArrayRead.java
@@ -2,7 +2,6 @@ package com.vietage.lang17.formatter.command;
 
 import com.vietage.lang17.formatter.IndentPrintStream;
 import com.vietage.lang17.parser.ast.expression.ArrayRead;
-import com.vietage.lang17.parser.ast.expression.OperatorPrecedence;
 
 import java.util.Queue;
 
@@ -17,15 +16,8 @@ public class FormatArrayRead extends FormatCommand {
 
     @Override
     public void format(IndentPrintStream out, Queue<FormatCommand> commandQueue) {
-        commandQueue.add(new InsertText(
-                indent,
-                arrayRead.getName() + "["
-        ));
-        commandQueue.add(new FormatExpression(
-                indent,
-                arrayRead.getIndexExpression(),
-                OperatorPrecedence.OR
-        ));
+        commandQueue.add(new InsertText(indent, arrayRead.getName() + "["));
+        commandQueue.add(new FormatExpression(indent, arrayRead.getIndexExpression()));
         commandQueue.add(new InsertText(indent, "]"));
     }
 }
