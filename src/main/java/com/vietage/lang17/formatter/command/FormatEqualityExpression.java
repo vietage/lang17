a7faaa648be119819cb952657374323da9bcafaa
Alexey Shilov <vietopk@gmail.com>
2018-02-17 13:00:09 +0300

    implemented FormatEqualityExpression

--- src/main/java/com/vietage/lang17/formatter/command/FormatEqualityExpression.java
+++ src/main/java/com/vietage/lang17/formatter/command/FormatEqualityExpression.java
@@ -1,20 +1,50 @@
 package com.vietage.lang17.formatter.command;
 
-import com.vietage.lang17.formatter.IndentPrintStream;
 import com.vietage.lang17.parser.ast.expression.EqualityExpression;
+import com.vietage.lang17.parser.ast.expression.EqualityOperator;
+import com.vietage.lang17.parser.ast.expression.Expression;
 import com.vietage.lang17.parser.ast.expression.OperatorPrecedence;
 
 import java.util.Queue;
 
 public class FormatEqualityExpression extends FormatOperatorExpression {
 
+    private final EqualityExpression equalityExpression;
+
     public FormatEqualityExpression(int indent, EqualityExpression equalityExpression,
                                     OperatorPrecedence parentOperatorPrecedence) {
-        super(indent);
+        super(indent, parentOperatorPrecedence);
+        this.equalityExpression = equalityExpression;
     }
 
     @Override
-    public void format(IndentPrintStream out, Queue<FormatCommand> commandQueue) {
+    protected Expression getExpression() {
+        return equalityExpression;
+    }
 
+    @Override
+    protected void addFormatCommands(Queue<FormatCommand> commandQueue) {
+        commandQueue.add(new FormatExpression(
+                indent,
+                equalityExpression.getLeftExpression(),
+                equalityExpression.getOperatorPrecedence()
+        ));
+        commandQueue.add(new InsertText(indent, formatEqualityOperator(equalityExpression.getOperator())));
+        commandQueue.add(new FormatExpression(
+                indent,
+                equalityExpression.getRightExpression(),
+                equalityExpression.getOperatorPrecedence()
+        ));
+    }
+
+    private String formatEqualityOperator(EqualityOperator operator) {
+        switch (operator) {
+            case EQUAL:
+                return " = ";
+            case NOT_EQUAL:
+                return " != ";
+            default:
+                throw new RuntimeException("Unsupported equality operator: " + operator);
+        }
     }
 }
