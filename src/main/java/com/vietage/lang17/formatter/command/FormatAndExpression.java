22415622422bf79e769444e003357204b6a4bca6
Alexey Shilov <vietopk@gmail.com>
2018-02-17 13:13:43 +0300

    implemented FormatAndExpression

--- src/main/java/com/vietage/lang17/formatter/command/FormatAndExpression.java
+++ src/main/java/com/vietage/lang17/formatter/command/FormatAndExpression.java
@@ -1,19 +1,42 @@
 package com.vietage.lang17.formatter.command;
 
-import com.vietage.lang17.formatter.IndentPrintStream;
 import com.vietage.lang17.parser.ast.expression.AndExpression;
+import com.vietage.lang17.parser.ast.expression.Expression;
 import com.vietage.lang17.parser.ast.expression.OperatorPrecedence;
 
 import java.util.Queue;
 
 public class FormatAndExpression extends FormatOperatorExpression {
 
-    public FormatAndExpression(int indent, AndExpression andExpression, OperatorPrecedence parentOperatorPrecedence) {
-        super(indent);
+    private final AndExpression andExpression;
+
+    public FormatAndExpression(int indent, AndExpression andExpression,
+                               OperatorPrecedence parentOperatorPrecedence) {
+        super(indent, parentOperatorPrecedence);
+        this.andExpression = andExpression;
     }
 
     @Override
-    public void format(IndentPrintStream out, Queue<FormatCommand> commandQueue) {
+    protected Expression getExpression() {
+        return andExpression;
+    }
 
+    @Override
+    protected void addFormatCommands(Queue<FormatCommand> commandQueue) {
+        boolean first = true;
+
+        for (Expression expression : andExpression.getExpressions()) {
+
+            if (!first) {
+                commandQueue.add(new InsertText(indent, " && "));
+            }
+            first = false;
+
+            commandQueue.add(new FormatExpression(
+                    indent,
+                    expression,
+                    andExpression.getOperatorPrecedence()
+            ));
+        }
     }
 }
