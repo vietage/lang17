006edb8b2859248d1c282665b670c722421aaee3
Alexey Shilov <vietopk@gmail.com>
2018-02-17 13:04:00 +0300

    implemented FormatArrayRead

--- src/main/java/com/vietage/lang17/formatter/command/FormatArrayRead.java
+++ src/main/java/com/vietage/lang17/formatter/command/FormatArrayRead.java
@@ -2,17 +2,30 @@ package com.vietage.lang17.formatter.command;
 
 import com.vietage.lang17.formatter.IndentPrintStream;
 import com.vietage.lang17.parser.ast.expression.ArrayRead;
+import com.vietage.lang17.parser.ast.expression.OperatorPrecedence;
 
 import java.util.Queue;
 
 public class FormatArrayRead extends FormatCommand {
 
+    private final ArrayRead arrayRead;
+
     public FormatArrayRead(int indent, ArrayRead arrayRead) {
         super(indent);
+        this.arrayRead = arrayRead;
     }
 
     @Override
     public void format(IndentPrintStream out, Queue<FormatCommand> commandQueue) {
-
+        commandQueue.add(new InsertText(
+                indent,
+                arrayRead.getName() + "["
+        ));
+        commandQueue.add(new FormatExpression(
+                indent,
+                arrayRead.getIndexExpression(),
+                OperatorPrecedence.OR
+        ));
+        commandQueue.add(new InsertText(indent, "]"));
     }
 }
