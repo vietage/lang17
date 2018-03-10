3b2d0eaa87b353328bd768a7cd6dc9f3fcbd6a9b
Alexey Shilov <vietopk@gmail.com>
2018-03-10 11:28:41 +0300

    implemented break statement interpretation

--- src/main/java/com/vietage/lang17/interpreter/state/Break.java
+++ src/main/java/com/vietage/lang17/interpreter/state/Break.java
@@ -1,4 +1,20 @@
 package com.vietage.lang17.interpreter.state;
 
+import com.vietage.lang17.interpreter.Runtime;
+
 public class Break implements State {
+
+    @Override
+    public void run(Runtime runtime) {
+        while (runtime.hasState()) {
+            State state = runtime.getState();
+
+            if (state instanceof While) {
+                runtime.exitState();
+                return;
+            } else {
+                runtime.exitState();
+            }
+        }
+    }
 }
