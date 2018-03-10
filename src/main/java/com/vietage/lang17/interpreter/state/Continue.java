b815939ba3f2d4e6b957aa67728fd1bfcc63ea9f
Alexey Shilov <vietopk@gmail.com>
2018-03-10 11:26:47 +0300

    implemented continue statement interpretation

--- src/main/java/com/vietage/lang17/interpreter/state/Continue.java
+++ src/main/java/com/vietage/lang17/interpreter/state/Continue.java
@@ -1,4 +1,19 @@
 package com.vietage.lang17.interpreter.state;
 
+import com.vietage.lang17.interpreter.Runtime;
+
 public class Continue implements State {
+
+    @Override
+    public void run(Runtime runtime) {
+        while (runtime.hasState()) {
+            State state = runtime.getState();
+
+            if (state instanceof While) {
+                return;
+            } else {
+                runtime.exitState();
+            }
+        }
+    }
 }
